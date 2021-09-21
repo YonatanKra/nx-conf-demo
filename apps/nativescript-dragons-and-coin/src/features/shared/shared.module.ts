06382b182460b644b2b34bdd239f6d08aaab170d
yonatankra <yonatan.kra@vonage.com>
2021-09-21 21:44:50 +0300

    Rename the folder

--- apps/nativescript-dragons-and-coin/src/features/shared/shared.module.ts
+++ apps/nativescript-dragons-and-coin/src/features/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptFormsModule, NativeScriptRouterModule } from '@nativescript/angular';
-import { DraongsAndCoinGameModule } from '../../app/draongs-and-coin-game/draongs-and-coin-game.module';
+import { DraongsAndCoinGameModule } from '../../app/dragons-and-coin-game/draongs-and-coin-game.module';
 
 const MODULES = [
   NativeScriptCommonModule,
