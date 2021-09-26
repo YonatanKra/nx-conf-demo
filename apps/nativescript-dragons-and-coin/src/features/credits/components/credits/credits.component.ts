7abe1ffd92256dd164992812c6355cf534394cb4
yonatankra <yonatan.kra@vonage.com>
2021-09-26 14:17:03 +0300

    Can now navigate from the credits page

--- apps/nativescript-dragons-and-coin/src/features/credits/components/credits/credits.component.ts
+++ apps/nativescript-dragons-and-coin/src/features/credits/components/credits/credits.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Page } from '@nativescript/core';
 import { FullScreenService } from '../../../shared/services/full-screen.service';
 import Credits from '../../../../assets/credits.json';
+import { RouterExtensions } from '@nativescript/angular';
 
 @Component({
   selector: 'credits',
@@ -12,11 +13,15 @@ export class CreditsComponent implements OnInit {
 
   credits = Credits;
 
-  constructor(private page:Page, private fullScreenService: FullScreenService) { }
+  constructor(private page:Page, private fullScreenService: FullScreenService, private routerExtensions: RouterExtensions) { }
 
   ngOnInit(): void {
     this.page.actionBarHidden = true;
     this.fullScreenService.goFullscreen();
+  }
+
+  async backHome() {
+    await this.routerExtensions.navigate(['']);
   }
 
 }
